e470e860699b7a4b2582c030addf36320ee7e214
kk08321 <77192930+kk08321@users.noreply.github.com>
2025-12-31 16:44:19 +0900

    Fix: TypeScriptエラーを修正し、GitHub Actionsでビルドステップを明示的に追加

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,5 +1,4 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
 import { controlWrapper, iconContainer, icon, messageWrapper, fretLabel } from "../styles/sheetPageStyles";
 
 interface ControlPanelProps {
